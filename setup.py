19affada2aea155b5787d0e5f8deb835e979febf
seisvelas <alej.alvarado94@gmail.com>
2020-09-09 19:56:06 -0500

    test

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'homograph',
   packages = ['homograph'],
-  version = '0.04',
+  version = '0.99',
   license='MIT',
   description = 'IDN Homograph detection tools',
   author = 'Alex Alvarado',
   author_email = 'alex@flowcrypt.com',
   url = 'https://github.com/seisvelas/idn-homograph-database',
   # Note: The below release doesn't exist yet!
-  download_url = 'https://github.com/seisvelas/idn-homographs-database/archive/0.02.tar.gz',
+  download_url = 'https://github.com/seisvelas/idn-homographs-database/archive/0.99.tar.gz',
   keywords = ['IDN', 'homograph', 'attack', 'security'],
   install_requires = [],
   include_package_data = True,
